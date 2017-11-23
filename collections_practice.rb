c16ee74341a66eb84e95eebce8c60cdd2bf4257f
Jason Vance <jbvance@gmail.com>
2017-11-23 14:42:24 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,9 +40,9 @@ end
 
 def add_s(array)
   new_array = []
-  array.each_with_index |element| do
+  array.each_with_index {|element, index|
     element += "s" if index != 1
     new_array.push(element)
-  end
+  }
   new_array
 end
